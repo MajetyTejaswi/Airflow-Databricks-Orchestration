1f9598655a80771f6575524ac0dde2482d079a1d
teju <majetytejaswi123@gmail.com>
2025-12-02 21:40:55 -0600

    deleted duplicate data source in maim.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -289,9 +289,6 @@ resource "aws_s3_bucket_versioning" "airflow_logs" {
   }
 }
 
-# Get current AWS account ID
-data "aws_caller_identity" "current" {}
-
 # Output information
 output "ec2_public_ip" {
   value       = aws_instance.airflow.public_ip
